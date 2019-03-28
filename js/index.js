cedd5ffa4546633a064f22831ed72a7d58154d25
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-03-28 11:27:56 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 $("#boton").click(function(){
-    $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId=1", type: "GET", success: function(result){
+    /*
+    $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').value, type: "GET", success: function(result){
       $("#div1").html(result);
     }});
+    */
+   $.get("https://jsonplaceholder.typicode.com/comments?postId=1", function(){ $('#div1').load() });
 });
