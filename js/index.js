1e6be0e352c89b4488b9aea7cf557af722d7c5a6
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-03-28 16:34:58 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
+function reset() {
+    $('#texto').val(null);
+}
+
+$('#formulario').submit(function(){
+    $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').val(), type: "GET", success: function(result){
+        $("#div1").html(JSON.stringify(result));
+    }});
+    return false;
+});
+
+/*
 $("#boton").click(function(){
     $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').val(), type: "GET", success: function(result){
         $("#div1").html(JSON.stringify(result));
     }});
 });
+*/
+
+window.onload = reset;
