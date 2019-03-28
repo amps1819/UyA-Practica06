852db3f923a36c3f1cd03d4c717540ac26d4196f
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-03-28 17:16:11 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,18 @@
+// Limpia los campos de texto
 function reset() {
     $('#texto').val(null);
 }
 
+// Hace la petición desde el submit del formulario
 $('#formulario').submit(function(){
-    $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').val(), type: "GET", success: function(result){
-        $("#div1").html(JSON.stringify(result));
+    $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').val(), type: "GET", success: function(respuesta){
+        for (var i = 0; i < respuesta.length; i++) {
+            $("#tabla").append("<tr><td>"+respuesta[i]["name"]+"</td><td>"+respuesta[i]["email"]+"</td></tr>");
+        }
     }});
+    // Para cancelar el submit "oficial"
     return false;
 });
 
-/*
-$("#boton").click(function(){
-    $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').val(), type: "GET", success: function(result){
-        $("#div1").html(JSON.stringify(result));
-    }});
-});
-*/
-
+// Cuando termina de cargar la página reseteamos los textos
 window.onload = reset;
