84c6d345abaf8cb00e73b1573271c4544b3ac341
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-04-04 10:46:28 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,13 +6,15 @@ function reset() {
 // Hace la petición desde el submit del formulario
 $('#formulario').submit(function(){
     $.ajax({url: "https://jsonplaceholder.typicode.com/comments?postId="+$('#texto').val(), type: "GET", success: function(respuesta){
-        $("#parrafo").append("<table>")
+        $("#parrafo")
+        .append("<table>")
         .append("<thead><tr><th>PostID</th><th>ID</th><th>Name</th><th>Email</th><th>Body</th></tr></thead>")
         .append("<tbody>");
         for (var i = 0; i < respuesta.length; i++) {
             $("#parrafo").append("<tr><td>"+respuesta[i]["postId"]+"</td><td>"+respuesta[i]["id"]+"</td><td>"+respuesta[i]["name"]+"</td><td>"+respuesta[i]["email"]+"</td><td>"+respuesta[i]["body"]+"</td></tr>");
         }
-        $("#parrafo").append("</tbody>")
+        $("#parrafo")
+        .append("</tbody>")
         .append("</table>");
         /*
         $("#parrafo").append("<table>");
